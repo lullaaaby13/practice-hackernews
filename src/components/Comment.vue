06c82546b3a64d1ad94691988be3c02d11bf1fb3
BlueCitron <sds901234@naver.com>
2019-06-25 23:23:42 +0900

    [feature_3] : Comment 추가

---
+++ src/components/Comment.vue
@@ -0,0 +1,36 @@
+<template id="">
+  <div>
+    <v-subheader>
+      <router-link
+      class="success--text mr-2"
+      :to="{ name: 'about' }">
+        {{ comment.user }}
+      </router-link>
+      {{ comment.time_ago }}
+    </v-subheader>
+
+    <span v-if="replyFor != null" class="grey--text">@{{ replyFor }}</span>
+    <div v-html="comment.content"></div>
+
+    <div
+    v-for="child in comment.comments"
+    style="padding-left: 30px;"
+    >
+      <Comment
+      :comment="child"
+      :replyFor="comment.user"
+      ></Comment>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  props: {
+    comment: Object,
+    replyFor: String,
+  },
+}
+</script>
--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -18,10 +18,32 @@
   <v-divider class="my-3"></v-divider>
 
   <div v-html="$store.state.item.content"></div>
+
+  <div>
+    <v-textarea
+    box
+    label="Comment"
+    ></v-textarea>
+    <v-btn color="success">ADD COMMENT</v-btn>
+  </div>
+
+  <v-divider class="my-3"></v-divider>
+
+  <template v-for="comment in $store.state.item.comments">
+    <Comment
+    :comment="comment"
+    ></Comment>
+  </template>
+
 </v-content>
 </template>
 <script>
+import Comment from '@/components/Comment.vue'
+
 export default {
+  components: {
+    Comment
+  },
   methods: {
     moveBack () {
       this.$router.go(-1)
