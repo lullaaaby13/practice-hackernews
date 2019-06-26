ce15d11945ba13e4e4a2022d843abbf0b067fa5d
BlueCitron <sds901234@naver.com>
2019-06-26 19:51:07 +0900

    [feature_5] : User 정보 출력

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,6 +1,24 @@
 <template id="">
 <v-content>
-  User.vue
+  <v-layout justify-start>
+    <v-flex md3>
+      <v-card class="pa-3">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">{{ $store.state.user.id }}</h3>
+          </div>
+        </v-card-title>
+
+        <v-divider></v-divider>
+
+        <v-card-text>
+          <h4>CREATED AT : {{ $store.state.user.created }}</h4>
+          <h4>KARMA : {{ $store.state.user.karma }}</h4>
+        </v-card-text>
+
+      </v-card>
+    </v-flex>
+  </v-layout>
 </v-content>
 </template>
 <script>
